7657cee34dce7712c5430751fd4f60e6631191fc
KevinWennerstroem <kevin.wennerstroem@pm.me>
2022-12-01 18:25:08 +0100

    Add Day01

---
+++ Day01/Program.cs
@@ -0,0 +1,41 @@
+﻿var input = await File.ReadAllTextAsync(@"inventory\calories.txt");
+
+var elveLists = input.Split("\r\n\r\n");
+
+var elveCalories = new int[elveLists.Length];
+
+for (int i = 0; i < elveLists.Length; i++)
+{
+    var sum = 0;
+    foreach(var item in elveLists[i].Split("\r\n"))
+    {
+        sum += Int32.Parse(item);
+    }
+    elveCalories[i] = sum;
+}
+
+int first = 0, second = 0, third = 0;
+
+foreach(var item in elveCalories)
+{
+    if (item > first)
+    {
+        second = first;
+        first = item;
+    }else if (item > second)
+    {
+        third = second;
+        second = item;
+    }else if (item > third)
+    {
+        third = item;
+    }
+}
+
+var top3Sum = first + second + third;
+
+Console.Write("Elve with the most calories: " + first);
+Console.Write("Top 3 Eleves in sum: " + top3Sum);
+
+
+Console.Read();
