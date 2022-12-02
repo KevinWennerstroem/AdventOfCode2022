e2b11aa4ef0a5d116b39d9981d876cac682ff755
KevinWennerstroem <kevin.wennerstroem@pm.me>
2022-12-02 21:40:56 +0100

    Add Day02

---
+++ Day02/Program.cs
@@ -0,0 +1,58 @@
+﻿var strategy = await File.ReadAllLinesAsync(@"inventory\plan.txt");
+
+int points = 0;
+
+foreach (var item in strategy)
+{
+    var choose = item.Split(" ");
+
+    if (choose[1] == "X")
+    {
+        switch (choose[0])
+        {
+            case "A":
+                points += 3;
+                break;
+            case "B":
+                points += 1;
+                break;
+            case "C":
+                points += 2;
+                break;
+        }
+    }else if (choose[1] == "Y")
+    {
+        points += 3;
+        switch (choose[0])
+        {
+            case "A":
+                points += 1;
+                break;
+            case "B":
+                points += 2;
+                break;
+            case "C":
+                points += 3;
+                break;
+        }
+    }else if (choose[1] == "Z")
+    {
+        points += 6;
+        switch (choose[0])
+        {
+            case "A":
+                points += 2;
+                break;
+            case "B":
+                points += 3;
+                break;
+            case "C":
+                points += 1;
+                break;
+        }
+    }
+}
+
+Console.WriteLine("Points: " + points);
+
+Console.Read();
