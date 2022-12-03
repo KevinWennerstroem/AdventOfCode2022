521618d2ef8da35c3ed2c63e31c5b84573409388
KevinWennerstroem <kevin.wennerstroem@pm.me>
2022-12-03 19:42:13 +0100

    Add Day03

---
+++ Day03/Program.cs
@@ -0,0 +1,81 @@
+﻿var rucksacks = await File.ReadAllLinesAsync(@"inventory\rucksack.txt");
+
+int prioSum = 0;
+
+foreach (var rucksack in rucksacks)
+{
+    var firstItem = rucksack.Substring(0, rucksack.Length/2);
+    var secondItem = rucksack.Substring(rucksack.Length/2);
+
+    foreach (var character in firstItem)
+    {
+        if (secondItem.Contains(character))
+        {
+            int index = 1;
+            if (Char.IsLower(character))
+            {
+                for (char i = 'a'; i <= 'z'; i++)
+                {
+                    if (i == character)
+                        break;
+                    else
+                        index++;
+                }
+            }else
+            {
+                index += 26;
+                for (char i = 'A'; i <= 'Z'; i++)
+                {
+                    if (i == character)
+                        break;
+                    else
+                        index++;
+                }
+            }
+
+            prioSum += index;
+            break;
+        }
+    }
+}
+
+int groupSum = 0;
+
+for (int r = 0; r < rucksacks.Length; r+=3)
+{
+    foreach (var character in rucksacks[r])
+    {
+        if(rucksacks[r+1].Contains(character) && rucksacks[r+2].Contains(character))
+        {
+            int index = 1;
+            if (Char.IsLower(character))
+            {
+                for (char i = 'a'; i <= 'z'; i++)
+                {
+                    if (i == character)
+                        break;
+                    else
+                        index++;
+                }
+            }else
+            {
+                index += 26;
+                for (char i = 'A'; i <= 'Z'; i++)
+                {
+                    if (i == character)
+                        break;
+                    else
+                        index++;
+                }
+            }
+
+            groupSum += index;
+            break;
+        }
+    }
+}
+
+Console.WriteLine("Priorities Sum: " + prioSum);
+Console.WriteLine("Group Sum: " + groupSum);
+
+Console.Read();
